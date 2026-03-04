51e13e1075166341f4773d43e4b9afbeea3cfe62
“pgetech-rhmg” <pge@robbmorgan.com>
2026-03-04 08:41:57 -0800

    updated

--- paige-mcp/test/test_mcp.py
+++ paige-mcp/test/test_mcp.py
@@ -4,7 +4,7 @@ from mcp import ClientSession
 from mcp.client.sse import sse_client
 
 async def test():
-    async with sse_client("http://localhost:8000/sse") as (read, write):
+    async with sse_client("https://paige-mcp-dev.nonprod.pge.com/sse") as (read, write):
         async with ClientSession(read, write) as session:
             await session.initialize()
             
