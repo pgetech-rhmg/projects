ba8fefa42d246cd3bb566422e387326872c11761
“pgetech-rhmg” <pge@robbmorgan.com>
2026-03-04 10:42:50 -0800

    updated

---
+++ pge-examples/s3_static_website/outputs.tf
@@ -0,0 +1,15 @@
+output "s3_bucket_name" {
+  description = "s3 bucket name"
+  value       = module.s3.id
+}
+
+output "s3_bucket_arn" {
+  description = "s3 ARN. Will be of format arn:aws:s3:::bucketname"
+  value       = module.s3.arn
+}
+
+
+output "website_endpoint" {
+  description = "The website endpoint"
+  value       = module.s3_static_website.website_endpoint
+}
