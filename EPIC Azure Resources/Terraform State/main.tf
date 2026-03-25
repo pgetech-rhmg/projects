669212b455776ba8d683d69fbbd4dc28d0ef5e31
Robb Morgan <rxla@chevron.com>
2026-03-25 10:40:03 -0700

    updated

--- EPIC Azure Resources/Terraform State/main.tf
+++ EPIC Azure Resources/Terraform State/main.tf
@@ -45,8 +45,9 @@ resource "azurerm_storage_account" "this" {
 ############################################
 
 resource "azurerm_storage_container" "tfstate" {
-  name               = var.container_name
-  storage_account_id = azurerm_storage_account.this.id
+  name                  = var.container_name
+  storage_account_name  = azurerm_storage_account.this.name
+  container_access_type = "private"
 }
 
 ############################################
